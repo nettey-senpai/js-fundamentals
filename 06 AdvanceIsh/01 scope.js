bde7e461485711fdfe0215961d8fb93b476dc384
Jeffrey <netteyjeff@gmail.com>
2025-03-03 10:59:12 +0000

    scope solution in js

---
+++ 06 AdvanceIsh/01 scope.js
@@ -0,0 +1,9 @@
+console.log(name);
+
+var name = "jeffrey"; // var would give you an undefined error
+
+
+if (true) {
+    let lastName = "Nettey" // let would not allow it to be accessed outside the scope unlike var
+}
+console.log(lastName);
