c27666c233ba8732f107aebcf2615586a5a5588b
Jeffrey <netteyjeff@gmail.com>
2025-03-03 11:17:48 +0000

    template literals in js

---
+++ 06 AdvanceIsh/02 strings.js
@@ -0,0 +1,8 @@
+var name = "jeffrey";
+var  fullName = `${name} nettey`
+
+console.log(fullName);
+console.log(fullName.charAt(0));
+console.log(fullName.endsWith("y"));
+console.log(fullName.includes("jeffrey"));
+console.log(fullName.toUpperCase());
