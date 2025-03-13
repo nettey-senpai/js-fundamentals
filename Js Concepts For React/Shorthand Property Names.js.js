0d356142d39e478f6f2713e655d336b4328f58d6
Jeffrey <netteyjeff@gmail.com>
2025-03-13 10:31:49 +0000

    shorthand property names

---
+++ Js Concepts For React/Shorthand Property Names.js.js
@@ -0,0 +1,12 @@
+const id = 1;
+const title = "Product 1";
+const rating = 5;
+
+// Only possible when object key names are the same as property names passed 
+const product = {
+  id,
+  title,
+  rating,
+};
+
+console.log(product);
