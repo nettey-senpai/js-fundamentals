5df4394cc6ca64918ac30c2f6dd9f89139c58ff7
Jeffrey <netteyjeff@gmail.com>
2025-03-13 10:55:39 +0000

    Default Parameters

---
+++ Js Concepts For React/Default Parameters.js
@@ -0,0 +1,7 @@
+function sum(numOne = 1, numTwo = 2) {
+  return numOne + numTwo;
+}
+
+let result = sum(); // if no values are passed here it would use the default values you set
+
+console.log(result, "result");
