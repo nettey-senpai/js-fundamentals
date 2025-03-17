cf260fbc5d770ea4412e619f03b2870c47627cc5
Jeffrey <netteyjeff@gmail.com>
2025-03-17 16:36:50 +0000

    promises

--- Asynchronous JS in Depth/index.js
+++ Asynchronous JS in Depth/index.js
@@ -1,49 +1,8 @@
-console.log("Before");
-getGroceryList(1)
-  .then(list => getGroceryItems(list.id))
-  .then(items => checkItemAvailability(items[0]))
-  .then(availability => console.log("Availability", availability))
-  .catch(err => console.error(err));
-console.log("After");
-
-function handleGroceryItems(list) {
-  getGroceryItems(list.id, handleItemAvailability);
-}
-
-function handleItemAvailability(items) {
-  checkItemAvailability(items[0], displayAvailability);
-}
-
-function displayAvailability(availability) {
-  console.log("Availability", availability);
-}
-
-function getGroceryList(id) {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      // Simulating a call to the database
-      console.log("Fetching grocery list from the database...");
-      resolve({ id: id, name: "Weekly groceries" });
-    }, 2000);
-  });
-}
-
-function getGroceryItems(id) {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      // Simulating a call to the database
-      console.log("Fetching grocery items...");
-      resolve(["Milk", "Bread", "Eggs"]);
-    }, 2000);
-  });
-}
-
-function checkItemAvailability(item) {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      // Simulating a call to the database
-      console.log("Checking item availability...");
-      resolve({ item: item, available: true });
-    }, 2000);
-  })
-}
+const p = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject(new Error("error message"))
+  }, 2000);
+});
+p
+  .then(res => console.log(res))
+  .catch(err => console.log(err.message));
