e6acab74f244e3fec5e65e326a6fbeac05c7b0db
Jeffrey <netteyjeff@gmail.com>
2025-03-17 16:20:31 +0000

    using named functions

--- Asynchronous JS in Depth/index.js
+++ Asynchronous JS in Depth/index.js
@@ -1,61 +1,39 @@
-// Incorrect Synchronous Code
-// console.log("before");
-// CANNOT get grocery item like this
-// const groceryItem = getGroceryItem(1);
-// console.log(groceryItem); // undefined
-// console.log("after");
+console.log("Before");
+getGroceryList(1)
+  .then(list => getGroceryItems(list.id))
+  .then(items => checkItemAvailability(items[0]))
+  .then(availability => console.log("Availability", availability))
+  .catch(err => console.error(err));
+console.log("After");
 
-// function getGroceryItem(id) {
-//   setTimeout(() => {
-//     // simulate a call to the database
-//     console.log("Reading a grocery item from the database...");
-//     return { id: id, name: "Apples" };
-//   }, 2000);
-// }
-
-// Callbacks Pattern Solution
-console.log("before");
-// getGroceryList(1, (groceryList) => {
-//   getGroceryItem(groceryList.id, (items) => {
-//     checkItemAvailability(items[0]);
-//   });
-// });
-// getGroceryList(1)
-//   .then((list) => getGroceryItem(list.id))
-//   .then((items) => checkItemAvailability(items[0]))
-//   .then((availability) => console.log("Availability", availability))
-//   .catch((err) => console.error(err));
-
-displayAvailability();
-
-console.log("after");
-
-async function displayAvailability() {
-  try {
-    const list = await getGroceryList(1);
-    const items = await getGroceryItem(list.id);
-    const availability = await checkItemAvailability(items[0]);
-    console.log("Availability", availability);
-  } catch (err) {
-    console.error(err);
-  }
+function handleGroceryItems(list) {
+  getGroceryItems(list.id, handleItemAvailability);
 }
 
-async function getGroceryList(id) {
+function handleItemAvailability(items) {
+  checkItemAvailability(items[0], displayAvailability);
+}
+
+function displayAvailability(availability) {
+  console.log("Availability", availability);
+}
+
+function getGroceryList(id) {
   return new Promise((resolve) => {
     setTimeout(() => {
-      // simulate a call to the database
+      // Simulating a call to the database
       console.log("Fetching grocery list from the database...");
-      resolve({ id: id, items: ["apple", "bananas", "bread"] });
+      resolve({ id: id, name: "Weekly groceries" });
     }, 2000);
   });
 }
 
-function getGroceryItem(id) {
+function getGroceryItems(id) {
   return new Promise((resolve) => {
     setTimeout(() => {
-      console.log("Fetching a grocery item from the grocery list...");
-      resolve(["apples", "bananas", "bread"]);
+      // Simulating a call to the database
+      console.log("Fetching grocery items...");
+      resolve(["Milk", "Bread", "Eggs"]);
     }, 2000);
   });
 }
@@ -63,42 +41,9 @@ function getGroceryItem(id) {
 function checkItemAvailability(item) {
   return new Promise((resolve) => {
     setTimeout(() => {
-      console.log("Checking availability...");
-      resolve(true);
+      // Simulating a call to the database
+      console.log("Checking item availability...");
+      resolve({ item: item, available: true });
     }, 2000);
-  });
+  })
 }
-
-// Promise Pattern Solution
-// console.log("before");
-// getGroceryItem(1)
-//   .then((item) => console.log(item))
-//   .catch((err) => console.error(err));
-// console.log("after");
-
-// function getGroceryItem(id) {
-//   return new Promise((resolve, reject) => {
-//     setTimeout(() => {
-//       console.log("Reading a grocery item from a database");
-//       resolve({ id: id, name: "Apple" });
-//     }, 2000);
-//   });
-// }
-
-// Async/Await
-// console.log("before");
-// async function displayGroceryItem() {
-//   const groceryItem = await getGroceryItem(1);
-//   console.log(groceryItem);
-// }
-// displayGroceryItem(1);
-// console.log("after");
-
-// function getGroceryItem(id) {
-//   return new Promise((resolve, reject) => {
-//     setTimeout(() => {
-//       console.log("Reading a grocery item from a database");
-//       resolve({ id: id, name: "Apple" });
-//     }, 2000);
-//   });
-// }
