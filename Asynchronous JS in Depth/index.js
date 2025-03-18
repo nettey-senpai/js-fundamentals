38760325f67e1787c3b3b8e37db8ad03d9570514
Jeffrey <netteyjeff@gmail.com>
2025-03-18 10:31:08 +0000

    Async js demo

--- Asynchronous JS in Depth/index.js
+++
@@ -1,8 +0,0 @@
-const p = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    reject(new Error("error message"))
-  }, 2000);
-});
-p
-  .then(res => console.log(res))
-  .catch(err => console.log(err.message));
