96ac57e5ba91503c9b91fc2aae3ee444c5165f1d
Jeffrey <netteyjeff@gmail.com>
2025-03-18 11:05:14 +0000

    using promises syntax to solve callback hell

--- Async JS in Depth/Callback Hell.js
+++ Async JS in Depth/Callback Hell.js
@@ -1,39 +1,44 @@
 // Callbacks Pattern Solution
 console.log("before");
 // callback hell
-getGroceryList(1, (groceryList) => {
-  getGroceryItem(groceryList.id, (items) => {
-    checkItemAvailability(items[0]);
-  });
-});
+// getGroceryList(1, (groceryList) => {
+//   getGroceryItem(groceryList.id, (items) => {
+//     checkItemAvailability(items[0]);
+//   });
+// });
+
+getGroceryList(1)
+    .then(list => getGroceryItem(list.id))
+    .then(items => checkItemAvailability(items[0]))
+    .then(availability => console.log("Availability",availability))
+    .catch(err => console.error(err));
+    
 console.log("after");
 
 function getGroceryList(id, callback) {
-  setTimeout(() => {
-    // simulate a call to the database
-    console.log("Fetching groceryy list from the database...");
-    callback({ id: id, items: ["Apples", "Bananas", "Bread"] });
-  }, 2000);
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      // simulate a call to the database
+      console.log("Fetching groceryy list from the database...");
+      resolve({ id: id, items: ["Apples", "Bananas", "Bread"] });
+    }, 2000);
+  });
 }
 
 function getGroceryItem(id, callback) {
-  setTimeout(() => {
-    console.log("Fetching a grocery item from a grocery list...");
-    callback(["Apples", "Bananas", "Bread"]);
-  }, 2000);
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      console.log("Fetching a grocery item from a grocery list...");
+      resolve(["Apples", "Bananas", "Bread"]);
+    }, 2000);
+  });
 }
 
 function checkItemAvailability(item) {
-  setTimeout(() => {
-    console.log("Checking availability");
-    return true;
-  }, 2000);
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      console.log("Checking availability");
+      resolve(true);
+    }, 2000);
+  });
 }
-
-
-console.log("Before");
-const list = getGroceryList(1);
-const items = getGroceryItem(list.id);
-const availability = checkItemAvailability(item[0]);
-console.log("After");
-
